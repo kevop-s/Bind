daea825ad159045a8887f0970a6b32ae5b4f2d41
kev <kev.gomez97@hotmail.com>
2020-05-30 15:41:38 -0500

    Ejemplo de configuracion de una zona

---
+++ ejemplos/kevops.zone.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#############################################
+#                                           #
+#      E.g. Configuración de una zona       #
+#                                           #
+#############################################
+
+# Creamos la vista para la zona que daremos de alta
+cat<<-EOF > /var/containers/$BIND_CONTAINER/var/named/views/views.conf
+view "external-zone" {
+    match-clients { any; };
+    zone "kevops.com" IN {
+       type master;
+       file "zones/kevops.com.zone";
+     };
+};
+EOF
+
+# Creamos el archivo con los registros correspondientes a la zona
+cat<<-EOF > /var/containers/$$BIND_CONTAINER/var/named/zones/kevops.com.zone
+$TTL    3600
+@       IN      SOA     kevops.com.  . (
+                1      ; Serial
+                10800   ; Refresh
+                3600    ; Retry
+                3600    ; Expire
+                1)      ; Minimum
+                IN NS  ns0
+                IN A   1.2.3.4
+ns0             IN A   1.2.3.4
+example         IN A   1.2.3.5
+EOF
