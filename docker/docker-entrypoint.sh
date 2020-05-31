700172f8ec252c880967bd7f64e986b64a0f88ad
kev <kev.gomez97@hotmail.com>
2020-05-31 11:12:20 -0500

    Verificacion dnssec

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,6 +5,8 @@ MAIN_PROC_RUN=1
 BIND_CONFIG_FILE="/etc/named/named.conf"
 BIND_CONFIG_FILE_BCK="/etc/named.conf"
 
+trap "docker_stop" SIGINT SIGTERM
+
 if [ -z "${BIND_KEY_ALGORITHM}" ]; then
         BIND_KEY_ALGORITHM="hmac-md5"
     fi
@@ -13,7 +15,13 @@ if [ -z "${BIND_CPU}" ]; then
     BIND_CPU=1
 fi
 
-trap "docker_stop" SIGINT SIGTERM
+HOSTNAME=$(cat /etc/hostname | awk -F. '{print $1}')
+VERIFY_DNSSEC=$(cat /etc/rndc.key | grep $HOSTNAME -o)
+if [ -z ${VERIFY_DNSSEC} ]; then
+    KEY_FILE=$(dnssec-keygen -a ${BIND_KEY_ALGORITHM} -b 512 -n HOST ${HOSTNAME})
+    PRIV_KEY=$(cat ${KEY_FILE}.private | awk '/Key/{print $2}')
+    sed -i /etc/rndc.key -e "s/key-name/${HOSTNAME}/" -e "s/key-algorithm/${BIND_KEY_ALGORITHM}/" -e "s#key-value#${PRIV_KEY}#"
+fi
 
 function docker_stop {
     echo "[BIND $(date +'%Y-%m-%d %R')] Rcv end signal"
@@ -55,14 +63,6 @@ EOF
 
     if [ ! -f "/var/named/views/views.conf" ]; then
         touch /var/named/views/views.conf
-    fi
-
-    HOSTNAME=$(cat /etc/hostname | awk -F. '{print $1}')
-    VERIFY_DNSSEC=$(cat /etc/rndc.key | grep $HOSTNAME -o)
-    if [ -z ${VERIFY_DNSSEC} ]; then
-        KEY_FILE=$(dnssec-keygen -a ${BIND_KEY_ALGORITHM} -b 512 -n HOST ${HOSTNAME})
-        PRIV_KEY=$(cat ${KEY_FILE}.private | awk '/Key/{print $2}')
-        sed -i /etc/rndc.key -e "s/key-name/${HOSTNAME}/" -e "s/key-algorithm/${BIND_KEY_ALGORITHM}/" -e "s#key-value#${PRIV_KEY}#"
     fi
 }
 
